65373585485b531b16ff4ab72c78b72fb19833fe
manaralamri <manaralamre33@gmail.com>
2024-07-11 16:17:43 +0300

    blog

--- Blog/models.py
+++ Blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    date_published = models.DateTimeField(auto_now_add=True)
+    date_published = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.title
